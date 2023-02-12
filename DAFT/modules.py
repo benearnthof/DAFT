391c1d5842f860374cec49df9ff50b70d505a427
benearnthof <benearnthof@hotmail.de>
2023-02-12 22:00:33 +0100

    update modules.py with FILM and DAFT
    Now includes FiLM block meta class and DAFT as an extension to it.

--- DAFT/modules.py
+++ DAFT/modules.py
@@ -1,3 +1,6 @@
+from abc import ABCMeta, abstractmethod
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 import torch.nn.parallel 
@@ -96,4 +99,167 @@ class FiLM(nn.Module):
 # betas is the v_shift parameter to shift the incoming feature map
 # DAFT uses a third `location` parameter in their code to specify the 
 # location in the network at which this operation is being done. 
+
+class FilmBase(nn.Module, metaclass=ABCMeta):
+  """
+  Wrapper for models using a Film-Like structure.
+  Very similar to ResBlock, but will sandwich in the Film scaling and shifting 
+  operation at the location specified.
+  """
+  # bn momentum left at default
+  # scale and shift left out since we always want to scale and shift
+  # activation using gelu as default, relu for convlayers
+  def __init__(self, in_channels, out_channels, stride, ndim_tabular, location):
+    super().__init__()
+    if location not in set(range(5)):
+      raise ValueError(f"Invalid location parameter, needs to be in {0, 1, 2, 3, 4}")
+    # initialize components for residual block
+    self.conv1 = conv3d(in_channels, out_channels, stride=stride)
+    self.bn1 = nn.BatchNorm3d(out_channels) # affine left at default of True
+    self.conv2 = conv3d(out_channels, out_channels)
+    self.bn2 = nn.BatchNorm3d(out_channels)
+    self.relu = nn.ReLU() # inplace left at default of False
+    self.global_pool = nn.AdaptiveAvgPool3d(1) # target output size of 1x1x1
+    # need to match dimensionality of input and output tensors for residual connection
+    if stride != 1 or in_channels != out_channels:
+      self.downsample = nn.Sequential(
+        conv3d(in_channels, out_channels, kernel_size=1, stride=stride),
+        nn.BatchNorm3d(out_channels) # no momentum
+      )
+    else:
+      self.downsample = None
+    # location specific dimensionality of Film output tensor
+    self.film_dims = 0
+    if location in {0, 1, 2}:
+      self.film_dims = in_channels
+    else: 
+      self.film_dims = out_channels
+    # activation function for Film layer
+    self.scale_activation = nn.GELU()
+
+  @abstractmethod
+  def rescale_features(self, feature_map, x_aux):
+    """Scale and shift features"""
+    raise NotImplementedError("Subclasses need to provide this method.")
+
+  def forward(self, feature_map, x_aux):
+    """
+    This is a bit messy and the reason this meta class exists.
+    Location defines the point at which we will rescale the image features
+    According to the tabular data. 
+    The exact procedure for this needs to be defined in `rescale_features`.
+    """
+    if self.location == 0:
+      feature_map = self.rescale_features(feature_map, x_aux)
+
+    residual = feature_map
+
+    if self.location == 1:
+      residual = self.rescale_features(residual, x_aux)
+    
+    if self.location == 2:
+      feature_map = self.rescale_features(feature_map, x_aux)
+
+    out = self.conv1(feature_map)
+    out = self.bn1(out)
+
+    if self.location == 3:
+      out = self.rescale_features(out, x_aux)
+    
+    out = self.relu(out)
+
+    if self.location == 4:
+      out = self.rescale_features(out, x_aux)
+
+    out = self.conv2(out)
+    out = self.bn2(out)
+
+    if self.downsample is not None:
+      residual = self.downsample(residual)
+    
+    out += residual
+    out = self.relu(out)
+    return out
+  
+
+class FilmBlock(FilmBase):
+  """
+  Class that extends FilmBase to include the scaling and shifting operation on 
+  the feature maps to account for tabular auxiliary data. 
+  In the original paper the auxiliary data had 15 features. 
+  """
+  def __init__(self, in_channels, out_channels, stride=2, ndim_tabular=15, location=0, bottleneck_dim=7):
+    super().__init__(
+      in_channels=in_channels,
+      out_channels=out_channels,
+      stride=stride,
+      ndim_tabular=ndim_tabular,
+      location=location,
+    )
+    self.bottleneck_dim = bottleneck_dim
+    # we always scale and shift
+    self.split_size = self.film_dims
+    self.film_dims = 2 * self.film_dims
+    # initialize embedding net for tabular data
+    embedding_layers = [
+      ("embedding_in", nn.Linear(ndim_tabular, self.bottleneck_dim, bias=False)),
+      ("embedding_relu", nn.ReLU()),
+      ("embedding_out", nn.Linear(self.bottleneck_dim, self.film_dims, bias=False)),
+    ]
+    self.embedding_net = nn.Sequential(OrderedDict(embedding_layers))
+
+  def rescale_features(self, feature_map, x_aux):
+    embedding = self.embedding_net(x_aux)
+    assert (embedding.size(0) == feature_map.size(0)) and (embedding.dim() == 2), f"Invalid output size of embedding network: {embedding.size()}"
+    scale, shift = torch.split(embedding, self.split_size, dim=1)
+    # independently obtain scale and shift as learnable vectors that match feature_map in dimension
+    scale = scale.view(*scale.size(), 1, 1, 1).expand_as(feature_map)
+    shift = shift.view(*shift.size(), 1, 1, 1).expane_as(feature_map)
+    # we always gate scale with the GELU activation
+    scale = self.scale_activation(scale)
+    # analog to `(gammas * x) + betas` in FiLM
+    return (scale * feature_map) + shift
+
+class DAFTBlock(FilmBlock):
+  """
+  Class that extends `FilmBlock` with a new `rescale_features` method.
+  The only change is added global pooling of the respective feature map
+  that is performed before concatenating the pooled information to the tabular
+  information and passing it through the embedding network
+  """
+  def __init__(self, in_channels, out_channels, stride=2, ndim_tabular=15, location=0, bottleneck_dim=7):
+    super().__init__(
+      in_channels=in_channels,
+      out_channels=out_channels,
+      stride=stride,
+      ndim_tabular=ndim_tabular,
+      location=location,
+      bottleneck_dim=bottleneck_dim,
+    )
+    # split size and film_dims initialized in superclass
+    # extend embedding input to match tabular dim + input from global pooling
+    embedding_input_dims = self.film_dims / 2 # TODO: double check if /2 is needed since we init film_dims in superclass
+    # initialize embedding net for tabular data
+    embedding_layers = [
+      ("embedding_in", nn.Linear(ndim_tabular + embedding_input_dims, self.bottleneck_dim, bias=False)),
+      ("embedding_relu", nn.ReLU()),
+      ("embedding_out", nn.Linear(self.bottleneck_dim, self.film_dims, bias=False)),
+    ]
+    self.embedding_net = nn.Sequential(OrderedDict(embedding_layers))
+
+  def rescale_features(self, feature_map, x_aux):
+    pooled_features = self.global_pool(feature_map)
+    pooled_features = pooled_features.view(pooled_features.size(0), -1)
+    pooled_features = torch.cat((pooled_features, x_aux), dim=1)
+
+    embedding = self.embedding_net(pooled_features)
+
+    scale, shift = torch.split(embedding, self.split_size, dim=1)
+    # scale and shift are learned independently
+    scale = scale.view(*scale.size(), 1, 1, 1).expand_as(feature_map)
+    shift = shift.view(*shift.size(), 1, 1, 1).expand_as(feature_map)
+    # we always gate shift with GELU
+    scale = self.scale_activation(scale)
+    # analog to `(gammas * x) + betas` in FiLM
+    return (scale * feature_map) + shift
 
